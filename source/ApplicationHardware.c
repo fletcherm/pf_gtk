cf6f862761771acd333431ab811bbb9924df8cd0
Matt Fletcher <fletcher@atomicobject.com>
2010-06-23 18:31:03 -0400

    destroy the window and quit when the x button is clicked

--- source/ApplicationHardware.c
+++ source/ApplicationHardware.c
@@ -1,6 +1,11 @@
 #include <gtk/gtk.h>
 #include "ApplicationHardware.h"
 
+static void destroy(GtkWidget *widget, gpointer data)
+{
+    gtk_main_quit();
+}
+
 void ApplicationHardware_Build() {
   GtkWidget *button = gtk_button_new_with_label("Hello World");
 
@@ -11,6 +16,7 @@ void ApplicationHardware_Build() {
   gtk_window_set_title(GTK_WINDOW(window), "Hello World with MCH");
   gtk_window_set_default_size(GTK_WINDOW(window), 300, 80);
   gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+  g_signal_connect(window, "destroy", G_CALLBACK (destroy), NULL);
 
   gtk_container_add(GTK_CONTAINER(window), box);
 
